b1a7e11e2e16f2d2ce1fbf196870e84f68b8ca80
Aer019 <vitalikrahmanov@gmail.com>
2025-04-23 00:11:54 +0300

    remove all lectures from master

--- Lecture 6/shoot_dice_easy.py
+++
@@ -1,8 +0,0 @@
-import random 
-
-user_result = int(input('Put 1 2 or 3 to play the dice -->'))
-comp_result = random.randint(1,4)
-
-win_or_not = 'you win' if user_result == comp_result \
-                else 'you lose'
-print(win_or_not)
